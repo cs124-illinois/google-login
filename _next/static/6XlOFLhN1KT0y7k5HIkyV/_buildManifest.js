6af770ad46b450d7de27a927fb4ff3caff1f7810
Geoffrey Challen <challen@illinois.edu>
2021-07-15 21:27:35 -0500

    Updates

---
+++ _next/static/6XlOFLhN1KT0y7k5HIkyV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b2497b14ae913a25f779.js"],"/_error":["static/chunks/pages/_error-06c7a441689a21c3eae6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
